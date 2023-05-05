d4649004a48e457e189f62d03172e077e858b248
Elton Campos <eltoncampos36@gmail.com>
2023-05-04 22:03:56 -0300

    test: category contructor

--- src/categories/domain/entities/category.spec.ts
+++ src/categories/domain/entities/category.spec.ts
@@ -1,8 +1,18 @@
-import {Category} from "./category"
+import { Category, CategoryProperties } from './category'
 
-describe("Category Tests", () => {
-    test("Constructor of category", () => {
-        const category = new Category("Movie")
-        expect(category.name).toBe("Movie")
+describe('Category Tests', () => {
+    test('Constructor of category', () => {
+        const props: CategoryProperties = {
+            name: 'Movie',
+            description: 'One description',
+            is_active: true,
+            created_at: new Date(),
+        }
+        const category = new Category(props)
+        expect(category.name).toBe(props.name)
+        expect(category.description).toBe(props.description)
+        expect(category.is_active).toBeTruthy()
+        expect(category.created_at).toBe(props.created_at)
+        expect(category.props).toStrictEqual(props)
     })
 })
