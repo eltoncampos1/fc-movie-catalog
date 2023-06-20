9f97a29e49eca3d142205fe5e5898819a0acf623
Elton Campos <eltoncampos36@gmail.com>
2023-06-20 20:03:38 -0300

    test: should accept a uuid passed in constructor and should generate an uuid if no id is passed on constructor

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -1,3 +1,5 @@
+import { validate as validateUUID } from 'uuid'
+
 import { InvalidUUIDError } from '../errors/invalid-uuid.error'
 import { UniqueEntityId } from './unique-entity-id.vo'
 
@@ -10,6 +12,31 @@ describe('UniqueEntityId unit Tests', () => {
         expect(() => new UniqueEntityId('fake id')).toThrow(
             new InvalidUUIDError()
         )
+        expect(validateSpy).toHaveBeenCalled()
+    })
+
+    it('should accept a uuid passed in constructor', () => {
+        const uuid = '8653512d-7657-4cf9-aaa1-7597116d0a5b'
+        const vo = new UniqueEntityId(uuid)
+
+        const validateSpy = jest.spyOn(
+            UniqueEntityId.prototype as any,
+            'validate'
+        )
+
+        expect(vo.id).toBe(uuid)
+        expect(validateSpy).toHaveBeenCalled()
+    })
+
+    it('should generate an uuid if no id is passed on constructor', () => {
+        const vo = new UniqueEntityId()
+
+        const validateSpy = jest.spyOn(
+            UniqueEntityId.prototype as any,
+            'validate'
+        )
+
+        expect(validateUUID(vo.id)).toBeTruthy()
         expect(validateSpy).toHaveBeenCalled()
     })
 })
