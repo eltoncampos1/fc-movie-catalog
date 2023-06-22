736d26237ba687fd05046001a37c2da74fbfa0fc
Elton Campos <eltoncampos36@gmail.com>
2023-06-22 14:30:05 -0300

    test:  convertsion toString

--- src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/value-object.spec.ts
@@ -3,12 +3,37 @@ import { ValueObject } from '../value-object'
 class StubValueObject extends ValueObject {}
 
 describe('ValueObject unit Tests', () => {
-    it('should set vakue', () => {
+    it('should set value', () => {
         const value = 'string value'
         let vo = new StubValueObject(value)
         expect(vo.value).toBe(value)
 
         vo = new StubValueObject({ prop1: 'value1' })
         expect(vo.value).toStrictEqual({ prop1: 'value1' })
+    })
+
+    it('should convert to a string', () => {
+        const date = new Date()
+        let arrange = [
+            { received: null, expected: 'null' },
+            { received: undefined, expected: 'undefined' },
+            { received: '', expected: '' },
+            { received: 'fake test', expected: 'fake test' },
+            { received: 0, expected: '0' },
+            { received: 1, expected: '1' },
+            { received: 5, expected: '5' },
+            { received: true, expected: 'true' },
+            { received: false, expected: 'false' },
+            { received: date, expected: date.toString() },
+            {
+                received: { prop: 'prop' },
+                expected: JSON.stringify({ prop: 'prop' }),
+            },
+        ]
+
+        arrange.forEach((value) => {
+            const vo = new StubValueObject(value.received)
+            expect(vo + '').toBe(value.expected)
+        })
     })
 })
